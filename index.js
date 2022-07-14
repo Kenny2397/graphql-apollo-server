c9b701fd58c4e17f99faf8682abe2ac80968a3d8
kenny <kenny.luque.t@uni.pe>
2022-07-14 11:31:59 -0500

    resolvers frontend operations

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const persons = [
     {
         name: 'Jose',
         phone: ' 3434-4234234',
+        age: 23,
         street: 'pasaje Testing',
         city: 'Ibiza',
         id: '6546ntjyn6unr6'
@@ -13,6 +14,7 @@ const persons = [
     {
         name: 'frank',
         phone: ' 3434-4234234',
+        age: 13,
         street: 'calle Testing',
         city: 'Ibiza',
         id: '45345347h65'
@@ -20,6 +22,7 @@ const persons = [
     {
         name: 'arevalo',
         phone: ' 3434-4734234',
+        age: 20,
         street: 'pasaje Hambuerger',
         city: 'Cali',
         id: '245grret6yuj456234'
@@ -27,6 +30,7 @@ const persons = [
     {
         name: 'Juan',
         phone: ' 34774-4234234',
+        age: 18,
         street: 'Calleee Testing',
         city: 'Cali',
         id: '245grerret6456234'
@@ -39,15 +43,19 @@ const typeDefs = gql`
     type Person {
         name: String!
         phone: String
+        age: Int
         street: String!
         city: String!
+        address: String!
+        canDrink: Boolean!
+        check: String!
         id: ID!
     }
     
     type Query {
         personCount: Int!
         allPersons: [Person]!
-        
+        findPerson(name: String!): Person
     }
 
 `
@@ -59,6 +67,20 @@ const resolvers = {
     Query: {
         personCount: () => persons.length,
         allPersons: () => persons,
+        findPerson: (root, args) => {
+            const { name } = args
+
+            return persons.find(person => person.name === name)
+        }
+    },
+    // Person: {
+    //     name: (root) => root.name
+    //     ...
+    // }
+    Person: {
+        address: (root) => `street: ${root.street}, city: ${root.city}`,
+        check: () => "kenny",
+        canDrink: (root) => root.age > 18
     }
 }
 
